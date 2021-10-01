dfc60f7ef70267bafaf35ddc3d7803de799772b6
Bruno Cabral <cabral.brc@gmail.com>
2021-10-01 12:37:49 -0700

    add second paddle

--- pong1.py
+++ pong1.py
@@ -16,8 +16,16 @@ paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.penup()
 paddle_a.goto(-350, 0) # start position
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 
 # Paddle B
+paddle_b = turtle.Turtle() # create turtle object
+paddle_b.speed(0) # animation speed, set to max
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350, 0) # start position
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 
 # Ball
 
