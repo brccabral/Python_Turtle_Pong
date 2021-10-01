ce5aac3a375eb5f5fbbd11793af765605f58e18f
Bruno Cabral <cabral.brc@gmail.com>
2021-10-01 12:35:38 -0700

    added first paddle

--- pong1.py
+++ pong1.py
@@ -9,6 +9,18 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0) # do not refresh screen, speeds up the game
 
+# Paddle A
+paddle_a = turtle.Turtle() # create turtle object
+paddle_a.speed(0) # animation speed, set to max
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0) # start position
+
+# Paddle B
+
+# Ball
+
 # Main game loop
 while True:
     wn.update()
